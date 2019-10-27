5c8b424b9c9756e4b18c193a3935b45e4bb11f84
vladimirjv <ivladimirjuarez@outlook.com>
2019-10-26 22:19:57 -0500

    User Query add Phone and Role

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -7,6 +7,8 @@ namespace SchoolSystem.DTOs
     public string LastName { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Role { get; set; }
     public string Password { get; set; }
   }
 }
